eb24bb75dacf4403a3c32e8cc5379523cea92d64
Brian Tobal <brian.tobal@gmail.com>
2019-04-17 10:20:18 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,8 +1,11 @@
 class Transfer
-  attr_accessor :sender, :receiver 
+  attr_accessor :sender, :receiver, :status 
   
-  def initialize 
-    
+  def initialize (transfer)
+    @transfer = transfer 
+    @sender = sender 
+    @receiver = receiver 
+    @status = 'pending'
   end     #def 
   
 end
